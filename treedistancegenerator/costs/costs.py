73c18f8dbc161e8f3b74c820f61d0d152f215b29
Eileen Kuehn <eileen.kuehn@kit.edu>
2017-04-03 18:40:55 +0200

    added better costs for move operation

--- treedistancegenerator/costs/costs.py
+++ treedistancegenerator/costs/costs.py
@@ -10,6 +10,14 @@ class EditDistanceCost(object):
 
 class TreeEditDistanceCost(EditDistanceCost):
     def __call__(self, node, operations, last_operation):
+        """
+        Move operation is supported naturally by considering a cost of 2 for the two nodes marked.
+
+        :param node:
+        :param operations:
+        :param last_operation:
+        :return:
+        """
         if last_operation == NO_OPERATION:
             return 0
         return 1
@@ -17,6 +25,16 @@ class TreeEditDistanceCost(EditDistanceCost):
 
 class FanoutWeightedTreeEditDistanceCost(EditDistanceCost):
     def __call__(self, node, operations, last_operation):
+        """
+        Move operation is supported naturally as two nodes get the assigned operation.
+        Thus, for each node first constant cost is summed and second last operation is evaluated.
+        Therefore, we get 2f + 2c for move operations (as defined).
+
+        :param node:
+        :param operations:
+        :param last_operation:
+        :return:
+        """
         cost = 0
         # if MOVE_OPERATION in operations or MOVE_OPERATION == last_operation:
         #     return NotImplemented
@@ -33,13 +51,15 @@ class FanoutWeightedTreeEditDistanceCost(EditDistanceCost):
 class SubtreeWeightedTreeEditDistanceCost(EditDistanceCost):
     def __call__(self, node, operations, last_operation):
         cost = 0
-        # if MOVE_OPERATION in operations or MOVE_OPERATION == last_operation:
-        #     return NotImplemented
+        if MOVE_OPERATION == last_operation:
+            return 1
         if NO_OPERATION != last_operation:
             cost += 1
         operations = set(operations)
         minimum_length = 1
         if NO_OPERATION in operations:
+            minimum_length += 1
+        if MOVE_OPERATION in operations:
             minimum_length += 1
         if len(operations) >= minimum_length:
             cost += 1
@@ -49,11 +69,11 @@ class SubtreeWeightedTreeEditDistanceCost(EditDistanceCost):
 class SubtreeHeightWeightedTreeEditDistanceCost(EditDistanceCost):
     def __call__(self, node, operations, last_operation):
         cost = 0
-        # if MOVE_OPERATION in operations or MOVE_OPERATION == last_operation:
-        #     return NotImplemented
+        if MOVE_OPERATION == last_operation:
+            return 1
         if NO_OPERATION != last_operation:
             cost += 1
         for index, operation in enumerate(operations):
-            if operation != NO_OPERATION:
+            if operation != NO_OPERATION and operation != MOVE_OPERATION:
                 cost += 1 / float(2**(len(operations) - index))
         return cost
